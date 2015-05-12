fc5654107f5e7f30d29ff0de035b340b988b5446
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2015-05-12 16:18:55 +0200

    Create deob_eval.py

---
+++ deob_eval.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+# Decode VBA Macro based on this obfuscator 
+# https://github.com/kkar/VBS-Obfuscator-in-Python
+#
+# Fafner [_KeyZee_]
+#
+
+import re
+import sys
+	
+for line in sys.stdin.readlines():
+        decoded = ""
+        line = line.rstrip()
+        encoded = line.split('*')
+        if (len(encoded)> 1):
+		for i in range(0, len(encoded) ):
+			decoded+=chr(eval(encoded[i]))
+		print decoded
+exit
