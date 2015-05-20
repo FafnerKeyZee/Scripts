bdcfda24863eb808e506ca2f8fd172c673e7244c
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2015-05-20 11:34:42 +0200

    Update deob_chr.py
    adding StrReverse(xxx)

--- VBS-VBA/deob_chr.py
+++ VBS-VBA/deob_chr.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python
+#Decode VBA Macro based on chr() obfuscation
+#Xavier Mertens <xavier@rootshell.be>
 #
-# Decode VBA Macro based on chr() obfuscation
-# Xavier Mertens <xavier@rootshell.be>
 #
-
-#
-# Modified version, original is there : 
-# https://github.com/xme/toolbox/blob/master/deobfuscate_chr.py
+#Modified version, original is there :
+#https://github.com/xme/toolbox/blob/master/deobfuscate_chr.py
 #
 
-"
-import re
-import sys
+# add reversestr parsing
 
-def do_chr(m):
-	if m.group(0):
-		return eval(re.sub(r'[cC][hH][rR][wW\$]*\(([\d\+\-\s.]*)\)',r'chr(int(\1))', m.group(0)))	
-	return ""
-	
-f = open(sys.argv[1], 'rb')
+import re 
+import sys 
+
+def reverseme(s):
+  s= s.encode('string_escape')
+  if s[len(s)-1] != ')':
+    return s[::-1]
+  return s[len(s)-2::-1]+')'
+  
+
+def do_chr (m):
+  if m.group (0) :
+    return eval(re.sub(r'[cC][hH][rR][wW\$]*\(([\d\+\-\s.]*)\)',r'chr(int(\1))', m.group(0))) 
+  return "" 
+
+def do_strme (m):
+  if m.group (0) :
+    return eval(re.sub(r'[sS][tT][rR][rR][eE][vV][eE][rR][sS][eE]\((.*)\)',r'reverseme("\1")', m.group(0))) 
+  return "" 
+
+f = open (sys.argv[1], 'rb') 
 for line in f.readlines():
-	line = re.sub(r'[cC][hH][rR][wW\$]*\(([\d+\+\-\s\.]*)\)', do_chr, line)
-	line = re.sub(" & ", "", line)
-	print line.rstrip()
-f.close()
+     line = re.sub (r'[cC][hH][rR][wW\$]*\(([\d+\+\-\s\.]*)\)', do_chr, line)
+     line = re.sub (" & ", "", line) 
+     line = re.sub ("& ", "", line)
+     line = re.sub (r'[sS][tT][rR][rR][eE][vV][eE][rR][sS][eE]\((.*)\)', do_strme, line)
+     line = line.rstrip ()
+     print line
+f.close () 
 exit
