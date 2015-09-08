87b6308422137191710f26793da28c21ffb58b59
Fafner [_KeyZee_] <FafnerKeyZee@users.noreply.github.com>
2015-09-08 17:33:33 +0200

    Update getfile.sh

--- Dyre/getfile.sh
+++ Dyre/getfile.sh
@@ -3,12 +3,12 @@ payload=(`cat db3.txt`)
 
 for ip in `cat db2.txt`
 do
-  valid=`curl -o /dev/null --silent --head --write-out '%{http_code}' $ip`
+  valid=`curl -o /dev/null --silent --head --write-out '%{http_code}' $ip -L -k`
   if [ "$valid" != "000" ]
   then
     for pay in "${payload[@]}"
     do
-      curl -o /dev/null --silent --head --write-out '%{http_code} %{url_effective}  \n' $ip/$pay --connect-timeout 10
+      curl -L -o /dev/null --silent --head --write-out '%{http_code} %{url_effective}  \n' $ip/$pay --connect-timeout 10 -k
     done
   fi
 done
